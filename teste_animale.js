f6c67e0134b791e0f1df18f70bfa9ac7af80b942
Thiago Farias <thiagofarias99@hotmail.com>
2021-06-29 15:35:20 -0300

    version 2.0

--- teste_animale.js
+++ teste_animale.js
@@ -1,128 +1,122 @@
-var $body = document.body;
+let $body = document.body;
 
-// PAGE VARIABLES
-var $img = document.querySelector('.product__image');
-var $productName = document.querySelector('.productName');
-var $skuBestPrice = document.querySelector('.skuBestPrice').innerText;
-var $price = parseInt($skuBestPrice.split("").filter(n => (Number(n))).join(""));
-var $totalDiscount = $price - (($price * 10) / 100);
-
-// ELEMENTS
-//// PRODUCT INFO
-$imgProd = `<img class="image_product" src="${$img.getAttribute('src')}" alt="Imagem do produto">`;
-$textTitle = `<div class="product_title">${$productName.innerText}</div>`;
-$textPrice = `<p>Preço: ${$skuBestPrice}</p>`;
-$textDiscount = `<p>Preço com desconto: R$ ${$totalDiscount.toFixed(2).replace('.',',')}</p>`;
-//// ACTIONS
-$buttonActPay = `<button class="button-pay button button--buy button--full product__add-to-cart active" href="javascript:;">Adicionar à Sacola</button>`;
-$btnClose = `<button class="btn-close-act">X</button>`;
-
-// DIV - BODY
-var textDiv = document.createElement('div');
-textDiv.setAttribute('class', 'product-footer');
-
-var divFooter = `
-    ${$imgProd}
-    <div class="actions-prod">
-        ${$btnClose}
-        ${$buttonActPay}
-    </div>
-    <div class="info-prod">
-        ${$textTitle}
-        ${$textPrice}
-        ${$textDiscount}
-    </div>`;
-textDiv.innerHTML = divFooter;
-
-$body.appendChild(textDiv);
-
-
-// DIVS - ELEMENTS
-$product_footer = document.querySelector('.product-footer');
-//// INFO
-$image_product = document.querySelector('.image_product');
-$info_product = document.querySelector('.info-prod');
-$product_title = document.querySelector('.product_title');
-//// ACTIONS
-$actions = document.querySelector('.actions-prod');
-$button_pay = document.querySelector('.button-pay');
-$button_close = document.querySelector('.btn-close-act');
-
-// STYLES
-//// MAIN - DIV
-$product_footer.style.display = "block";
-$product_footer.style.float = "left";
-$product_footer.style.background = "#fff";
-$product_footer.style.width = "100%";
-$product_footer.style.height = "120px";
-$product_footer.style.padding = "10px";
-$product_footer.style.position = "fixed";
-$product_footer.style.bottom = "0";
-$product_footer.style.borderTop = "1px solid #000";
-$product_footer.style.zIndex = "9";
-
-//// INFO - IMAGE
-$image_product.style.width = "80px";
-$image_product.style.height = "100px";
-$image_product.style.margin = "0 10px 0 30px";
-$image_product.style.float = "left";
-
-//// INFO - DIV
-$info_product.style.fontFamily = "sans-serif";
-$info_product.style.fontWeight = "bold";
-$info_product.style.background = "none";
-$info_product.style.position = "left";
-
-//// INFO - TITLE
-$product_title.style.lineHeight = "1.4";
-$product_title.style.margin = "10px 0 5px 0";
-$product_title.style.textTransform = "uppercase";
-
-//// ACTIONS - DIV
-$actions.style.float = "right";
-$actions.style.margin = "0 30px 50px 30px";
-$actions.style.display = "block";
-$actions.style.width = "30%";
-
-//// ACTIONS -BUTTON PAY
-$button_pay.style.width = "80%";
-$button_pay.style.margin = "7% 2% 0 0";
-$button_pay.style.float = "left";
-$button_pay.style.textTransform = "uppercase";
-
-//// ACTIONS - BUTTON CLOSE
-$button_close.style.border = "none";
-$button_close.style.background = "none";
-$button_close.style.fontWeight = "bold";
-$button_close.style.fontSize = "18px";
-$button_close.style.float = "right";
-
-// ACTIONS - EVENTS
-$button_pay.addEventListener('click', function() {
-    var balls = '...'
-    buttonText(0, balls.substr(2));
-    buttonText(200, balls.substr(1));
-    buttonText(500, balls.substr(0));
-
-    setTimeout(function() {
-        $button_pay.innerText = "Produto adicionado!";
-    }, 1000);
-    setTimeout(function() {
-        $button_pay.innerText = "Adicionar à Sacola";
-    }, 4000)
-});
-
-$button_close.addEventListener('click', function() {
-    $product_footer.style.display = "none";
-});
-
-function buttonText(time, text) {
-    setTimeout(function() {
-        $button_pay.style.fontSize = "12px";
-        $button_pay.innerText = text;
-    }, time)
+function newElement(tagName, className = '', href = null, src = null, text = null) {
+    const element = document.createElement(tagName)
+    if (className != '') element.className = className
+    element.href = href
+    element.src = src
+    element.innerText = text
+    return element
 }
 
-function openPF() {
-    $product_footer.style.display = "block";
+function createFooterElement() {
+    // PAGE VARIABLES
+    let $img = document.querySelector('.product__image');
+    let $productName = document.querySelector('.productName');
+    let $skuBestPrice = document.querySelector('.skuBestPrice').innerText;
+    let $price = parseFloat($skuBestPrice.split('').filter(n => (Number(n))).join(''));
+    let $totalDiscount = $price - (($price * 10) / 100);
+    console.log($totalDiscount)
+
+    // ELEMENTS
+    //// PRODUCT INFO
+    let $imgProd = newElement('img', 'image_product', '', `${$img.getAttribute('src')}`, '')
+    let $textTitle = newElement('div', 'product_title', null, null, `${$productName.innerText}`)
+    let $textPrice = newElement('p', '', null, null, `Preço: ${$skuBestPrice}`)
+    let $textDiscount = newElement('p', '', null, null, `Preço com desconto: R$ ${$totalDiscount.toFixed(2).replace('.',',')}`)
+    //// ACTIONS
+    let $buttonActPay = newElement('button', 'button-pay button button--buy button--full product__add-to-cart active', 'javascript:;', '', 'Adicionar à Sacola')
+    let $btnClose = newElement('button', 'btn-close-act', null, null, 'X')
+
+    // DIV - BODY
+    this.element = newElement('div', 'product-footer')
+
+    let actionsProd = newElement('div', 'actions-prod')
+    let infoProd = newElement('div', 'info-prod')
+
+    actionsProd.appendChild($btnClose)
+    actionsProd.appendChild($buttonActPay)
+
+    infoProd.appendChild($textTitle)
+    infoProd.appendChild($textPrice)
+    infoProd.appendChild($textDiscount)
+
+    this.element.appendChild($imgProd)
+    this.element.appendChild(actionsProd)
+    this.element.appendChild(infoProd)
+
+    $body.appendChild(this.element);
 }
+
+createFooterElement()
+
+function addStyles() {
+    // DIVS - ELEMENTS
+    let $product_footer = document.querySelector('.product-footer');
+    //// INFO
+    let $image_product = document.querySelector('.image_product');
+    let $info_product = document.querySelector('.info-prod');
+    $product_title = document.querySelector('.product_title');
+    //// ACTIONS
+    let $actions = document.querySelector('.actions-prod');
+    let $button_pay = document.querySelector('.button-pay');
+    let $button_close = document.querySelector('.btn-close-act');
+
+
+    function addStyle(element, properties) {
+        element.setAttribute('style', properties)
+    }
+    // STYLES
+    //// MAIN - DIV
+    addStyle($product_footer, 'display: block; float: left; background: #fff; width: 100%; height: 120px; padding: 10px; position: fixed; bottom: 0; border-top: 1px solid #000; z-index: 9;')
+
+    //// INFO - IMAGE
+    addStyle($image_product, 'width: 80px; height: 100px; margin: 0 10px 0 30px; float: left;')
+
+    //// INFO - DIV
+    addStyle($info_product, 'font-family: sans-serif; font-weight: bold; background: none; position: left;')
+
+    //// INFO - TITLE
+    addStyle($product_title, 'line-height: 1.4; margin: 10px 0 5px 0; text-transform: uppercase;')
+
+    //// ACTIONS - DIV
+    addStyle($actions, 'float: right; margin: 0 30px 50px 30px; display: block; width: 30%;')
+
+    //// ACTIONS -BUTTON PAY
+    addStyle($button_pay, 'width: 80%; margin: 7% 2% 0 0; float: left; text-transform: uppercase;')
+
+    //// ACTIONS - BUTTON CLOSE
+    addStyle($button_close, 'border: none; background: none; font-weight: bold; font-size: 18px; float: right;')
+
+    // ACTIONS - EVENTS
+    $button_pay.addEventListener('click', function() {
+        var balls = '...'
+        buttonText(0, balls.substr(2));
+        buttonText(200, balls.substr(1));
+        buttonText(500, balls.substr(0));
+
+        setTimeout(function() {
+            $button_pay.innerText = "Produto adicionado!";
+        }, 1000);
+        setTimeout(function() {
+            $button_pay.innerText = "Adicionar à Sacola";
+        }, 4000)
+    });
+
+    $button_close.addEventListener('click', function() {
+        $product_footer.style.display = "none";
+    });
+
+    function buttonText(time, text) {
+        setTimeout(function() {
+            $button_pay.style.fontSize = "12px";
+            $button_pay.innerText = text;
+        }, time)
+    }
+
+    function openPF() {
+        $product_footer.style.display = "block";
+    }
+}
+
+addStyles()
